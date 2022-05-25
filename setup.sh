84076b7cf933ca543358ca6bf54af3784f090766
Aleksandar Aleksandrov <vulko@ingim.io>
2022-05-25 12:10:30 +0100

    install bat ¯\_㋡_/¯

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ set -Eeuo pipefail
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/.dotfiles/z.sh ~/z.sh
 
+BAT_VERSION="v0.21.0/bat-v0.21.0-x86_64-unknown-linux-musl"
+wget -qO- https://github.com/sharkdp/bat/releases/download/${BAT_VERSION}.tar.gz | tar -xzvf - ${BAT_VERSION}/bat -C /tmp/bat
+sudo ln /tmp/${BAT_VERSION}/bat /usr/local/bin/bat
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
