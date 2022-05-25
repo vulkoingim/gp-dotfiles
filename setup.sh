74f2ac50a1e9427378f4b938bd0e2470e09f20c0
Aleksandar Aleksandrov <vulko@ingim.io>
2022-05-25 11:10:37 +0100

    (╯°Д°)╯︵/(.□ . \)

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -Eeuo pipefail
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
-cp .bash_aliases ~/.bash_aliases
+# symlink manually since gitpod skips this if you have setup.sh present
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 
-source ~/.bashrc
+# source ~/.bashrc
