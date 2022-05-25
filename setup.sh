d37cde342bf0502af9eb6c96d92d24b629ea281f
Aleksandar Aleksandrov <vulko@ingim.io>
2022-05-25 11:23:27 +0100

    (╯°□°)╯︵ ┻━┻

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-# ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
-cp ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+# cp ~/.dotfiles/.bash_aliases ~/.bash_aliases
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
-
-# source ~/.bashrc
