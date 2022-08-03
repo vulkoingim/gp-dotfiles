7ba04de89faf2a8525dafe01371a0ff6711d4362
Aleksandar Aleksandrov <a.g.aleksandrov@gmail.com>
2022-08-03 11:50:30 +0100

    (ノಠ益ಠ)ノ彡┻━┻

--- setup.sh
+++ setup.sh
@@ -40,4 +40,4 @@ git config --global color.status.changed yellow
 echo 'colorscheme gruvbox' >> ~/.vimrc
 echo 'set bg=dark' >> ~/.vimrc
 echo 'source ~/.bash_aliases' >> ~/.zshrc
-echo '[ -z "$ZSH_VERSION" ] && export SHELL=`which zsh` && exec "$SHELL" -l' >> ~/.bashrc
+#echo '[ -z "$ZSH_VERSION" ] && export SHELL=`which zsh` && exec "$SHELL" -l' >> ~/.bashrc
