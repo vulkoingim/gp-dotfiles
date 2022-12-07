a8784e6de2e356001ec3a9552bf2df7d61e79edc
Aleksandar Aleksandrov <a.g.aleksandrov@gmail.com>
2022-12-07 15:14:24 +0000

    ┳━┳ ヽ(ಠل͜ಠ)ﾉ

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ tar -xvf kubecolor_0.0.20_Linux_x86_64.tar.gz kubecolor -C ~/kubecolor
 sudo ln ~/kubecolor /usr/local/bin/kubecolor 2>/dev/null
 
 sudo apt-get update
-sudo apt-get --yes install tig zplug fzf zsh-syntax-highlighting fd-find exa
+sudo apt-get --yes install tig zplug fzf zsh-syntax-highlighting fd-find #exa
 
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.2/zsh-in-docker.sh)" -- \
    -t https://github.com/vulkoingim/geometry \
